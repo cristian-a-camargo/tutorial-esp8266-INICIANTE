c3c6e20f58d8a150c6c49a8648e94ef29379d8e4
cristian-a-camargo <cristianacamargo@restinga.ifrs.edu.br>
2017-10-18 02:56:36 -0200

    declarando variavel

---
+++ declarando.variavel/declarando.variavel.ino
@@ -0,0 +1,15 @@
+#define BOTAO 1 // constante
+const int pin_botao = D3; // constante
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+        
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+             int valor_x; // variável
+}
