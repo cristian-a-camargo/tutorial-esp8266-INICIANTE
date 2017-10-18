87319fd4ff75db8060adfe7fd18f9b71d54bcaf9
cristian-a-camargo <cristianacamargo@restinga.ifrs.edu.br>
2017-10-18 02:57:21 -0200

    serial print

---
+++ serial.print/serial.print.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+           Serial.begin(115200); // denifição de velocidade de tranmisão 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+            Serial.println("Ola, Aprendiz , seja bem vindo ao maravilhoso mundo do ESP-8266");
+
+
+            
+}
