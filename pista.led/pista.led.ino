9172037ec373528f0c9539fc994ecf16dc0f592b
cristian-a-camargo <cristianacamargo@restinga.ifrs.edu.br>
2017-10-18 02:44:31 -0200

    piscar led

---
+++ pista.led/pista.led.ino
@@ -0,0 +1,27 @@
+void setup() {
+  // put your setup code here, to run once:
+
+            //pinMode declarando o pino.
+            //OUTPUT declando que o pino é uma saida
+            // D3 , pino declarado.
+            // D4 , pino declarado.
+
+            pinMode ( D3 , OUTPUT );
+            pinMode ( D4 , OUTPUT );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+          //dizendo que o pino D3 ligara o led 
+          //dizendo que o pino D3 ligara o led 
+          
+          digitalWrite ( D3 , HIGH ); 
+          delay(2000);
+          digitalWrite ( D3 ,   LOW  );
+          delay(2000);
+           digitalWrite( D4 , HIGH ); 
+          delay(2000);
+          digitalWrite( D4 ,   LOW  );
+          delay(2000);
+}
