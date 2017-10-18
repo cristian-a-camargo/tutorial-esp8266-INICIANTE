f42d12278e31a94254a941e8df58bc5548c4e629
cristian-a-camargo <cristianacamargo@restinga.ifrs.edu.br>
2017-10-18 02:41:59 -0200

    acendendo 2 leds

---
+++ Ledligado.ino/Ledligado.ino.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+
+            //pinMode declarando o pino.
+            //OUTPUT declando que o pino é uma saida
+            // D3 , pino declarado.
+            // D4 , pino declarado.
+
+            pinMode ( D3 , OUTPUT );
+            pinMode ( D4 , OUTPUT );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+          //dizendo que o pino D3 ligara o led 
+          //dizendo que o pino D3 ligara o led 
+          
+          digitalWrite ( D3 , HIGH ); 
+          digitalWrite ( D4 , HIGH ); 
+}
